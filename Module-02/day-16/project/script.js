0d402d809e26408bc6012d4dd7bb15ddbe4a7e34
Elizabeth <ElizabethAbayZewdie@outlook.com>
2026-08-10 10:12:46 +0300

    Tele - Birr Homework

---
+++ Module-02/day-16/project/script.js
@@ -0,0 +1,17 @@
+function FizzBuzz(num){
+    let i = 1
+    while (i <= 100){
+        if (i % 3 === 0 && i % 5 === 0) console.log("TeleBirr");
+
+        else if (i % 3 === 0 )  console.log("Tele");
+        else if ( i % 5 === 0) console.log("Birr")
+        
+        i++
+    }
+    
+        
+    
+}
+
+
+FizzBuzz(100)
