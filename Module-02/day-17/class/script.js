3df7876bdb6afcf55593d9c827b7d43ed4d2dceb
Elizabeth <ElizabethAbayZewdie@outlook.com>
2026-08-10 11:15:55 +0300

    day 17

--- Module-02/day-17/class/script.js
+++ Module-02/day-17/class/script.js
@@ -1,6 +1,6 @@
 // const GreetFunc = function Hello(...names){
 //     for (let name of names){
-//         // in will take the index
+        // in will take the index
 //         console.log(`Hello ${name}`)
 //     }
 // }
@@ -18,7 +18,7 @@
 // greet("Eka") // prints Hello Eka
 
 
-// // function returning another function
+// function returning another function
 // const outer = (fame) => {
 //     let inter = 'Elsa'
 
@@ -28,7 +28,7 @@
 // }
 
 
-// // this wldnt cause logging - just associates the inner function to the variable inner
+// this wldnt cause logging - just associates the inner function to the variable inner
 // let inner = outer('ahdakjh');
 // inner()
 
@@ -58,3 +58,44 @@ let returned = Greeting2();
 //     returned()
 returned('Elais');
 returned('Elizabeth');
+
+
+
+// const Greeting2 = () => {
+// this is not higher order bc it returns a string
+//     let lastName = 'Abay'
+//     let inner = (fname) => {
+//         console.log(`${fname} ${lastName}`)
+//     }
+
+
+//     return `Hello ${inner('Me')}`
+// }
+
+
+
+
+
+// higher order function that accepts callback
+
+let cb = (msg) => console.log(msg)
+
+
+let higherOrderFunction = (endNumber , cb) => {
+    // cb = callBack
+    let i = 0
+    while (i <= endNumber){
+        if (i % 2 === 0){
+            // even
+            cb('even')
+        } else{
+            // odd
+            cb('odd')
+        }
+        
+        i++
+    }
+}
+
+
+higherOrderFunction(20 , cb)
